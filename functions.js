e309ad803a5c243d8a2a9daadd3f96c855eacd40
CluelessZaxc18 <ZaxcKheal@gmail.com>
2025-07-23 01:54:08 +0800

    Adding codes and files

---
+++ functions.js
@@ -0,0 +1,11 @@
+function checkLogin() {
+  const Email = document.getElementById("Email").value;
+  const Password = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  if (Email == "Zaxc@gmail.com" && Password == "Zaxc123") {
+    window.location.href = "Index.html";
+  } else {
+    message.textContent = "Wrong email or password. Try again.";
+  }
+}

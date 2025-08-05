30a5eb81d42dbc8534e0281e5dcd1fb4b75b8e07
CluelessZaxc18 <ZaxcKheal@gmail.com>
2025-08-05 09:59:51 +0800

    Changes some important stuff

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -1,7 +1,7 @@
 function checkLogin() {
   const Email = document.getElementById("Email").value;
   const Password = document.getElementById("password").value;
-  const message = document.getElementById("message");
+  const message = document.getElementById("message1");
 
   if (Email == "Zaxc@gmail.com" && Password == "Zaxc123") {
     window.location.href = "Index.html";
@@ -9,3 +9,19 @@ function checkLogin() {
     message.textContent = "Wrong email or password. Try again.";
   }
 }
+
+const messageBox = document.getElementById("message");
+const charCount = document.getElementById("charCount");
+const warning = document.getElementById("warning");
+
+messageBox.addEventListener("input", () => {
+  const currentLength = messageBox.value.length;
+  charCount.textContent = `${currentLength} / 150 characters`;
+
+  if (currentLength >= 150) {
+    warning.style.display = "block";
+  } else {
+    warning.style.display = "none";
+  }
+});
+
